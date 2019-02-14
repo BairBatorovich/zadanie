6f0d44232851559029c872f6d3daa50066369cf6
Bair <pinhaev17@gmail.com>
2019-02-14 14:37:17 +0800

    модальное окно удаления

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './Components.css';
 import News from "./News";
 import Cnews from "./Cnews";
-import Edit from "./Edit"
 import {Route} from "react-router-dom";
 
 class Body extends Component {
@@ -11,7 +10,6 @@ class Body extends Component {
             <div>
                 <Route path={"/cnews"} component={Cnews}/>
                 <Route path={"/news"} component={News}/>
-                <Route path={"/edit"} render = { ()=> <Edit /> }/>
             </div>
         );
     }
--- src/components/Npost.js
+++ src/components/Npost.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './Components.css';
 import {NavLink} from "react-router-dom";
+import Modal from 'react-modal';
 
 const deleteUrl="http://testapi.ibb.su/removeNews";
 const edtNews="http://testapi.ibb.su/setNews";
 const getdetNews="http://testapi.ibb.su/getNewsDetails";
+
 
 class Npost extends Component {
 
@@ -12,15 +14,20 @@ class Npost extends Component {
         super(props);
         this.state = {headerNews: ''};
         this.state = {textNews: ''};
-        this.state = {modalOpen: false};
+        this.state={modalIsOpen:false};
+        this.state={modalIsOpenEdit:false};
 
         this.delnews = this.delnews.bind(this);
+        this.editnews = this.editnews.bind(this);
+
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+
+        this.openModalEdit = this.openModalEdit.bind(this);
+        this.closeModalEdit = this.closeModalEdit.bind(this);
     };
 
-    handleOpen = () => this.setStat({modalOpen: true});
-    handleClose = () => this.setStat({modalOpen: false});
     delnews = () => {
-        if(window.confirm("Delete")) {
             fetch(deleteUrl, {
                 method: 'post',
                 headers: {
@@ -29,21 +36,67 @@ class Npost extends Component {
                 },
                 body: JSON.stringify({"id": this.props.idNews})
             });
-            alert("Новость удалена");
-        } else alert("Действие отменено");
+        this.closeModal();
     };
+    editnews =() => {
+        fetch(edtNews, {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({"id":"5c5ee5cde0b07548cc55f3bd", "header":"заголовок2222", "text":"Текст текст 2222"})
+        })
+    }
+
+    openModal() {
+        this.setState({modalIsOpen: true});
+    }
+    closeModal() {
+        this.setState({modalIsOpen: false});
+    }
+
+    openModalEdit() {
+        this.setState({modalIsOpenEdit: true});
+    }
+    closeModalEdit() {
+        this.setState({modalIsOpenEdit: false});
+    }
 
     render() {
         return (
             <div className="Npost">
                 <h5>{this.props.headerNews}</h5>
                 <p>{this.props.textN}</p>
-                <button onClick={this.delnews}><NavLink to={"/News"}>Удалить</NavLink></button>
-                <button><NavLink to={"/Edit"}>Редактировать</NavLink></button>
 
-                {/*<Modal isOpen={this.state.modalOpen}>
-                    <button onClick={this.state.handleOpen}>Кнопка</button>
-                </Modal>*/}
+                <button onClick={this.openModalEdit}>Редактировать</button>
+                <button onClick={this.openModal}>Удалить</button>
+
+                <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onAfterOpen={this.afterOpenModal}
+                    onRequestClose={this.closeModal}
+                    contentLabel="Example Modal"
+                    className="Modal"
+                >
+
+                    <h2>Удалить</h2>
+                    <button onClick={this.delnews}>Да</button>
+                    <button onClick={this.closeModal}>Отмена</button>
+                </Modal>
+
+                <Modal
+                    isOpen={this.state.modalIsOpenEdit}
+                    onRequestClose={this.closeModalEdit}
+                    contentLabel="Example Modal"
+                    className="ModalEdit"
+                >
+                    <h5>Редактирование</h5>
+                    <textarea>Заголовок</textarea>
+                    <button onClick={this.editnews()}>Да</button>
+                    <button onClick={this.closeModalEdit}>Нет</button>
+                </Modal>
+
             </div>
         );
     }
